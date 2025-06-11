144e24e57809bde83be6667a02a58e6b7a63e1df
Kawsar Ahmed <ahmed.official2211@gmail.com>
2025-06-11 18:11:57 +0100

    Completed the desktop version of navigation will do mobile version by tomorrow

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,32 +3,67 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import styled from "styled-components"
 import LogoImg from "../utils/images/Logo.webp"
 import Button from "./Button"
-import {SearchRounded, FavoriteBorder} from "@mui/icons-material"
+import {SearchRounded, FavoriteBorder, ShoppingCartOutlined} from "@mui/icons-material"
 /*
 If it is a component from styled-components then define
 like this const Nav = styled.div``; if it is from react-rouer-dom
 like nav link then enclose it with brackets const NavLinks = styled(NavLink)``;
 */
 const Nav = styled.div`
-    
+    background-color: ${({ theme }) => theme.bg};
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    position: sticky;
+    top: 0;
+    z-index: 10;
 `;
 const NavbarContainer = styled.div`
-    
+    width:100%;
+    max-width: 1400px;
+    display: flex;
+    gap: 14px;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 1rem;
 `;
 const NavLogo = styled.div`
-    
+    //width: 100%;
+    display: flex;
+    align-items: center;
+    color: inherit;
+    text-decoration: none;
 `;
 const Logo = styled.img`
     height: 34px;
 `;
-const NavItems = styled.div`
-    
+const NavItems = styled.ul`
+    display: flex;
+    gap: 20px;
+    @media screen and (max-width: 768px){
+        display: none;
+    }
 `;
-const NavLinks = styled(NavLink)`
-    
+const Navlink = styled(NavLink)`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+    border-bottom: 1.8px solid ${({ theme }) => theme.primary};
+  }
 `;
 const ButtonContainers = styled.div`
-    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    color: ${({theme}) => theme.primary};
+    gap: 28px;
+    @media screen and (max-width: 768px){
+        display: none;
+    }
 `;
 
 const Navbar = () => {
@@ -39,11 +74,11 @@ const Navbar = () => {
                 <Logo src={LogoImg} />
             </NavLogo>
             <NavItems>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/">Shop</NavLink>
-                <NavLink to="/">New Arrivals</NavLink>
-                <NavLink to="/">Orders</NavLink>
-                <NavLink to="/">Contact</NavLink>
+                <Navlink to="/">Home</Navlink>
+                <Navlink to="/">Shop</Navlink>
+                <Navlink to="/">New Arrivals</Navlink>
+                <Navlink to="/">Orders</Navlink>
+                <Navlink to="/">Contact</Navlink>
             </NavItems>
             <ButtonContainers>
                 <NavLink to="/search">
@@ -51,6 +86,9 @@ const Navbar = () => {
                 </NavLink>
                 <NavLink to="/favourites">
                     <FavoriteBorder/>
+                </NavLink>
+                <NavLink to="/cart">
+                    <ShoppingCartOutlined />                
                 </NavLink>
                 <Button text="SignIn" ></Button>
             </ButtonContainers>
